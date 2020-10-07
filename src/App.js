4b4b2740c3a09b250d0671acb2ceeb14d85a919d
Yaroslav <dombrovskiy91@gmail.com>
2020-10-07 22:10:18 +0300

    useContext()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Switcher from "./Switcher";
 
 function App() {
+    const MyContext = React.createContext();
+    const Child = () => {
+    const value = useContext(MyContext);
+    return <h2>{value}</h2>
+    };
+
   return (
     <div className="App">
-          <Switcher/>
+          {/*<Switcher/>*/}
+          <MyContext.Provider value="this is useContext() Hook baby">
+              <Child/>
+          </MyContext.Provider>
     </div>
   );
 }
