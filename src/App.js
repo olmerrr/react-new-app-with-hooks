d6ecb8f5015910b9756ddd0c04a3fcc7741eff89
Yaroslav <dombrovskiy91@gmail.com>
2020-10-09 16:32:01 +0300

    useEffect() + fetch API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect,useState} from "react";
 
 function App() {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState(1);
     const [visible, setVisible] = useState(true);
 
 
@@ -11,7 +11,8 @@ function App() {
                 <button onClick={() => setValue(v => v + 1)}>+</button>
                 <button onClick={() => setVisible(false)}>hide</button>
                 {/*<HookCounter value={value}/>*/}
-                <Notification/>
+                {/*<Notification/>*/}
+                <PlanetInfo id = {value}/>
             </div>
         )
     }else {
@@ -19,6 +20,17 @@ function App() {
     }
 
 };
+const PlanetInfo = ({id}) => {
+    let canseled = false;
+    const [name, setName] = useState('null');
+    useEffect(() =>{
+        fetch(`http://swapi.dev/api/planets/${id}/`)
+            .then(res => res.json())
+            .then(data =>!canseled && setName(data.name));
+    return () => canseled = true;
+    },[id])
+    return <p>{id}  - {name} </p>
+}
 // const HookCounter = ({value}) => {
 //     useEffect(() => {
 //         console.log('mount');
@@ -27,20 +39,20 @@ function App() {
 //     useEffect(() => console.log('update'));
 //     return <p>{value}</p>
 // };
-const Notification = () => {
-    const [visible,setVisible] = useState(true);
-
-    useEffect( () =>{
-      const timeOut = setTimeout(()=>
-            setVisible(false),2500);
-      return () => clearTimeout(timeOut);
-    },[]);
-
-    if(visible){
-        return(
-            <p>Hello</p>
-        )
-    };
-
-}
+// const Notification = () => {
+//     const [visible,setVisible] = useState(true);
+//
+//     useEffect( () =>{
+//       const timeOut = setTimeout(()=>
+//             setVisible(false),2500);
+//       return () => clearTimeout(timeOut);
+//     },[]);
+//
+//     if(visible){
+//         return(
+//             <p>Hello</p>
+//         )
+//     };
+//
+// }
 export default App;
